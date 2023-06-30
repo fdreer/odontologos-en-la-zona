974664714f5cc5ddd07ec12f7a18d0a18f59f566
Franco Dreer <francodreer@gmail.com>
2023-06-30 18:03:28 -0300

    configurando deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/odontologos-en-la-zona">
       <App />
     </BrowserRouter>
   </ThemeProvider>
