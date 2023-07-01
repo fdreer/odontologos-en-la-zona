db589212a54c1605fe048c9e9adda527e08028ae
Franco Dreer <francodreer@gmail.com>
2023-07-01 11:54:38 -0300

    change BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {ThemeProvider} from './contexts/ThemeContext.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, HashRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider>
-    <BrowserRouter basename="/odontologos-en-la-zona">
+    <HashRouter basename="/odontologos-en-la-zona">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ThemeProvider>
 )
