ac739209035cc604381957cfd598a18169e825ad
Franco Dreer <francodreer@gmail.com>
2023-07-01 11:09:12 -0300

    correccion url base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/odontologos-en-la-zona/',
+  base: '/odontologos-en-la-zona',
   plugins: [react()],
 })
