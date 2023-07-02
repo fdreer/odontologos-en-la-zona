cec055531a7175fcda3444de21f26c87cf5971a6
Franco Dreer <francodreer@gmail.com>
2023-07-02 11:49:25 -0300

    styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom'
 import {styled} from 'styled-components'
 import ButtonTheme from './ButtonTheme/ButtonTheme'
+import {useRef, useState} from 'react'
 
 const HeaderStyled = styled.ul`
   padding: 25px;
@@ -19,33 +20,88 @@ const ButtonHeader = styled.section`
 `
 
 function Header({themeContext}) {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+  function handleOpen() {
+    setMenuIsOpen(!menuIsOpen)
+  }
+
   return (
-    <header className="main-header">
-      <nav className="navigation-page">
-        <i className="fa-solid fa-tooth tooth"></i>
-        <HeaderStyled>
-          <Link className="link" to="/">
-            <ButtonHeader>
-              <i className="fa-solid fa-house"></i>
-              <span>HOME</span>
-            </ButtonHeader>
-          </Link>
-          <Link className="link" to="/favoritos">
-            <ButtonHeader>
-              <i className="fa-solid fa-heart"></i>
-              <span>FAVORITOS</span>
-            </ButtonHeader>
-          </Link>
-          <Link className="link" to="/contacto">
-            <ButtonHeader>
-              <i className="fa-solid fa-address-book"></i>
-              <span>CONTACTO</span>
-            </ButtonHeader>
-          </Link>
-        </HeaderStyled>
+    <div className="main-header">
+      <header>
+        <Logo />
+        <NavBar />
         <ButtonTheme themeContext={themeContext} />
-      </nav>
-    </header>
+        <MenuBurger handleOpen={handleOpen} />
+        <MenuAside isOpen={menuIsOpen} />
+      </header>
+    </div>
+  )
+}
+
+function MenuBurger({handleOpen}) {
+  return (
+    <button className="btn-burger" onClick={handleOpen}>
+      <i className="fa-solid fa-bars burger"></i>
+    </button>
+  )
+}
+
+function MenuAside({isOpen}) {
+  return (
+    <aside className={`menu-aside ${isOpen ? '' : 'hidden'}`}>
+      <section>
+        <Link className="link" to="/">
+          <ButtonHeader>
+            <i className="fa-solid fa-house"></i>
+            <span>HOME</span>
+          </ButtonHeader>
+        </Link>
+        <Link className="link" to="/favoritos">
+          <ButtonHeader>
+            <i className="fa-solid fa-heart"></i>
+            <span>FAVORITOS</span>
+          </ButtonHeader>
+        </Link>
+        <Link className="link" to="/contacto">
+          <ButtonHeader>
+            <i className="fa-solid fa-address-book"></i>
+            <span>CONTACTO</span>
+          </ButtonHeader>
+        </Link>
+      </section>
+    </aside>
+  )
+}
+
+function Logo() {
+  return <i className="fa-solid fa-tooth tooth"></i>
+}
+
+function NavBar() {
+  return (
+    <nav className="navigation-page">
+      <HeaderStyled>
+        <Link className="link" to="/">
+          <ButtonHeader>
+            <i className="fa-solid fa-house"></i>
+            <span>HOME</span>
+          </ButtonHeader>
+        </Link>
+        <Link className="link" to="/favoritos">
+          <ButtonHeader>
+            <i className="fa-solid fa-heart"></i>
+            <span>FAVORITOS</span>
+          </ButtonHeader>
+        </Link>
+        <Link className="link" to="/contacto">
+          <ButtonHeader>
+            <i className="fa-solid fa-address-book"></i>
+            <span>CONTACTO</span>
+          </ButtonHeader>
+        </Link>
+      </HeaderStyled>
+    </nav>
   )
 }
 
